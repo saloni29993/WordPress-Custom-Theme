32ad8e11550c248bc71992c47c0d0cfb6d4f8fad
Saloni Shah <saloni29993@gmail.com>
2016-11-06 18:37:07 -0700

    Initial Commit

---
+++ custom-theme/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+add_theme_support('menus');
+
+add_theme_support('post-thumbnails');
+
+function wpcustom_excerpt_length($length) {
+	return 16;
+}
+add_action('excerpt_length', 'wpcustom_excerpt_length', 999);
+
+function register_theme_menus() {
+
+	register_nav_menus(
+		array(
+			'primary-menu' 	=> __( 'Primary Menu', 'sal-custom-theme' )			
+		)
+	);
+
+}
+add_action( 'init', 'register_theme_menus' );
+
+function wpcustom_theme_styles() {
+
+	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+	//wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+	wp_enqueue_style( 'font_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpcustom_theme_styles' );
+
+function wpcustom_theme_js() {
+
+	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );	
+	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
+	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );		
+
+}
+add_action( 'wp_enqueue_scripts', 'wpcustom_theme_js' );
+
+
+?>
---
+++ custom-theme/single.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+
+<section class="two-column row no-max pad">
+  <div class="small-12 columns">
+    <div class="row">
+      <!-- Primary Column -->
+      <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
+        <div class="primary">
+    
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        
+          <article class="post">
+
+            <h1><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h1>
+                <ul class="post-meta no-bullet">
+                  <li class="author">
+                      <span class="wpt-avatar small">
+                        <?php echo get_avatar(get_the_author_meta('ID'), 24); ?>
+                      </span>
+                      by <?php the_author_posts_link(); ?>
+                  </li>
+                  <li class="cat">in <?php the_category(','); ?></li>
+                  <li class="cat">on <?php the_time('F j, Y'); ?></li>
+                </ul> 
+
+                <?php if(the_post_thumbnail()) : ?>
+                  <div class="img-container">
+                    <?php the_post_thumbnail('large'); ?>
+                    <p>Photo by Gratt Spore</p>
+                  </div>  
+                <?php endif; ?>
+
+                <?php the_content(); ?>
+
+                <?php comments_template(); ?>
+
+          </article>    
+      
+        <?php endwhile; else : ?>
+      
+        <p><?php _e( 'Sorry, no pages found.' ); ?></p>
+      
+      <?php endif; ?>
+    
+    </div>  
+
+  <!-- Secondary Column -->
+  <div class="small-12 medium-4 medium-pull-8 columns">
+    <div class="secondary">
+      <h2 class="module-heading">Sidebar</h2>
+    </div>
+  </div>
+  
+  </div>
+</section>
+
+<?php get_footer(); ?>
